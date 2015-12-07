d301116084076df95e38d25987ef793a1fc82876
Daniel Goldman <dzgoldman@wesleyan.edu>
2015-12-07 14:31:21 -0500

    set up music file, tracking timing of 'notes' (which are currently just strings that get logged

--- public/app.js
+++ public/app.js
@@ -1,44 +1,71 @@
+$(function() {
 
-$(function () {
-    console.log('I am in here.');
-    var Engine = Matter.Engine,
+  console.log('I am in here.', test);
+  var Engine = Matter.Engine,
     World = Matter.World,
     Bodies = Matter.Bodies,
-    Render= Matter.render,
+    Render = Matter.render,
     Events = Matter.Events;
 
-// create a Matter.js engine
-var engine = Engine.create(document.body);
-console.log(engine.timing.timeScale=0.7)
+  // create a Matter.js engine
+  var engine = Engine.create(document.body);
+  engine.timing.timeScale = 1;
 
-var world = engine.world
+  var world = engine.world
 
-var circle = Bodies.circle(300, 40, 8, 1000);
-circle.restitution =1;
-circle.friction = 0;
-world.gravity.y=1;
-var bodies=[]
-bodies.push(circle)
-// add all of the bodies to the world
+  //make the player
+  var circle = Bodies.circle(300, 40, 8, 1000);
+  circle.restitution = 1;
+  circle.friction = 0;
+  world.gravity.y = 1;
+  var bodies = []
+  bodies.push(circle)
 
 
-var bottom = Bodies.rectangle(400, 600, 800,50, {isStatic: true})
-var top = Bodies.rectangle(400, 0, 800,50, {isStatic: true})
-var left = Bodies.rectangle(0, 300, 50 ,600, {isStatic: true})
- var right = Bodies.rectangle(800, 300, 50,600, {isStatic: true})
-bodies.push(bottom, top, left, right)
+  //make walls
+  var bottom = Bodies.rectangle(400, 600, 800, 50, {
+    isStatic: true
+  })
+  var top = Bodies.rectangle(400, 0, 800, 50, {
+    isStatic: true
+  })
+  var left = Bodies.rectangle(0, 300, 50, 600, {
+    isStatic: true
+  })
+  var right = Bodies.rectangle(800, 300, 50, 600, {
+    isStatic: true
+  })
+  bodies.push(bottom, top, left, right)
 
-World.add(world, bodies);
-engine.render.options.wireframes = false
-engine.render.options.background = 'grey';
+  World.add(world, bodies);
+  engine.render.options.wireframes = false
+  engine.render.options.background = 'grey';
 
-// run the engine
-Engine.run(engine);
+  // run the engine
+  Engine.run(engine);
 
-Events.on(engine, 'collisionEnd', function(event) {
-  //event has info about the two objects
-  console.log(event);
-})
+  Events.on(engine, 'collisionEnd', function(event) {
+    //event has info about the two objects
+
+  });
+
+  //playing notes
+
+  var currentNote = 0;
+  Events.on(engine, "afterTick", function(event) {
+      var currentTime = engine.timing.timestamp;
+
+    if (currentTime > timing[currentNote] * 1000) {
+      var xLoc = circle.position.x;
+      var yLoc = circle.position.y;
+
+      
+      console.log(notes[currentNote]);
+      currentNote+=1
+
+
+    }
+  })
 
 
 
---
+++ public/music.js
@@ -0,0 +1,5 @@
+console.log("Hello, Dave");
+var test = 42;
+
+var notes = ['c', 'd#', 'f', 'a', 'e']
+var timing = [1,  2,   3,     6,   9]
