3c43338e823f7e4fa6202353289b525aed9d947d
Daniel Goldman <dzgoldman@wesleyan.edu>
2016-01-07 12:44:44 -0500

    deleted npm version of decomp

--- public/js/title.js
+++ public/js/title.js
@@ -4,6 +4,7 @@ function title () {
     Bodies = Matter.Bodies,
     Render = Matter.render,
     Events = Matter.Events,
+    Vertices = Matter.Vertices,
     Vector = Matter.Vector,
     Composite = Matter.Composite,
     Body = Matter.Body,
@@ -29,18 +30,26 @@ function title () {
   world.bounds.max.x = width;
   world.gravity.y = 0.3;
 
+for (var i = 2; i <= 10; i++) {
+(function (i) {
 
-$.get('./design/SVGs/droptone-02.svg').done(function(data) {
+  var prefix = '0';
+  if (String(i).length==2) {
+    prefix=''
+  }
+
+
+$.get('./design/SVGs/droptone-'+prefix+ i +'.svg').done(function(data) {
 
            var vertexSets = [];
             var   color =  'black';
 
            $(data).find('path').each(function(i, path) {
-
-               vertexSets.push(Svg.pathToVertices(path, 30));
+             var points = Svg.pathToVertices(path, 30);
+            vertexSets.push(Vertices.scale(points, 0.4, 0.4));
            });
 
-           World.add(world, Bodies.fromVertices(400, 80, vertexSets, {
+           World.add(world, Bodies.fromVertices(200+(40*i), 80, vertexSets, {
                render: {
                    fillStyle: color,
                    strokeStyle: color
@@ -48,9 +57,10 @@ $.get('./design/SVGs/droptone-02.svg').done(function(data) {
                isStatic: true
            }, true));
        });
+     })(i)
+   } // end of for loop
 
-
-}
+} // end of title fucntion
 
 
 
