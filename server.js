f81928c7e701ebfaa632e5de28d3788554028b7f
Daniel Goldman <dzgoldman@wesleyan.edu>
2016-12-01 09:29:40 -0500

    added ev

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-
+require('dotenv').config({
+    silent: true
+});
 var express  = require('express');
 var app      = express();
 var morgan   = require ('morgan');
@@ -13,7 +15,7 @@ app.use(morgan('combined'));
 app.use(  express.static(__dirname+'/public'));
 app.use(bodyParser());
 
-mongoose.connect('mongodb://localhost/tunendrop', function(err) {
+mongoose.connect(process.env.MONGODB_URI, function(err) {
    if (err) {
       console.log(err);
    } else {
@@ -48,6 +50,6 @@ app.get('/recentlycreated', function (req, res) {
 
 
 // listen on a port
-app.listen(3000, function(){
+app.listen(process.env.PORT, function(){
   console.info('Listening on  port 3000...')
 })
